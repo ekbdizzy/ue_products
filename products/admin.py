3392f510e503c396c831beb9c7c9b58cac255cbd
Aleksei Koshkin <koshkin.pydev@gmail.com>
2024-07-30 21:08:53 +0300

    Add products models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Tariff, Promotion
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Tariff)
+admin.site.register(Promotion)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model for products."""
+
+    name = models.CharField(max_length=255, verbose_name='Name')
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
+
+class Tariff(models.Model):
+    """Model for tariffs."""
+
+    name = models.CharField(max_length=255, verbose_name='Name')
+    base_price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name='Base price'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='tariffs',
+        verbose_name='Product',
+    )
+
+    class Meta:
+        db_table = 'tariffs'
+        verbose_name = 'Tariff'
+        verbose_name_plural = 'Tariffs'
+
+    def __str__(self):
+        return self.name
+
+
+class Promotion(models.Model):
+    """Model for promotions."""
+
+    name = models.CharField(max_length=255, verbose_name='Name')
+    discount = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(99)],
+        verbose_name='Percentage of discount',
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='promotions',
+        verbose_name='Product',
+    )
+    start_date = models.DateField(verbose_name='Start date')
+    end_date = models.DateField(verbose_name='End date')
+    tariffs = models.ManyToManyField(
+        Tariff,
+        related_name='promotions',
+        verbose_name='Tariffs',
+    )
+
+    class Meta:
+        db_table = 'promotions'
+        verbose_name = 'Promotion'
+        verbose_name_plural = 'Promotions'
+
+    def __str__(self):
+        return self.name
